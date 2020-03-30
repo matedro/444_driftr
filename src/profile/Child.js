68f1cc6eb4e309ee97b64e219e828ee2e63a725b
kel2941 <kel2941@rit.edu>
2020-03-30 01:45:05 -0400

    Profile Creation Pages

---
+++ src/profile/Child.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Child.css';
+import Man from './mannn.jpg';
+function Child() { return (
+<div className="profileChild">
+    <header>
+        <h1>Child Profile Creation</h1>
+        <input type="submit" value="Logout" />
+    </header>
+        <img src={Man} alt="profile image" width="200" height="160" class="man"/>
+
+    <div className="inputContainer2">
+
+    </div>
+
+    <div className="bigWrapper">
+            <div className="inputRight">
+                <form>
+                    <label>
+                        Name:
+                        <input type="text" name="name" />
+                    </label>
+                </form>
+            </div>
+            <div className="inputRight">
+
+                <form>
+                    <label>
+                        Age:
+                        <input type="text" name="name" />
+                    </label>
+                </form>
+            </div>
+            <div className="inputRight">
+                <form>
+                    <label>
+                        Interests:
+                        <input type="text" name="name" />
+                    </label>
+                </form>
+            </div>
+            <div className="inputRight">
+                <form>
+                    <label>
+                        Gender:
+                        <input type="text" name="name" />
+                    </label>
+                </form>
+            </div>
+            <div className="inputRight">
+                    <form>
+                        <label>
+                            Medical History:
+                            <input type="text" name="name" />
+                        </label>
+                    </form>
+            </div>
+            <div className="inputRight">
+                    <form>
+                        <label>
+                            Sexual Orientation:
+                            <input type="text" name="name" />
+                        </label>
+                    </form>
+            </div>
+            <div className="inputRight">
+                    <form>
+                        <label>
+                            Nationality:
+                            <input type="text" name="name" />
+                        </label>
+                    </form>
+            </div>
+    </div>
+    <div className="button">
+        <input type="submit" value="Save" />
+    </div>
+</div>
+); } export default Child;
