1550c1816534d64316981a532e04bffef4c0a885
matedro <matedro1@gmail.com>
2020-03-30 10:32:51 -0400

    Improved Filter

--- src/evaluation/Filter.js
+++ src/evaluation/Filter.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function Filter() {
   return (
-    <p>This is a filter</p>
+    <div>
+      <h1>Filters</h1>
+      <label>Age: <input size={2} /> - <input size={2} /></label>
+      <label>Nationality:</label>
+      <select>
+        <option value="">Select one...</option>
+      </select>
+      <input type="checkbox" name="male" />
+      <label for="male">M</label>
+      <input type="checkbox" name="female" />
+      <label for="female">F</label>
+    </div>
   )
 }
 
