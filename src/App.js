cef643f92cc7e29d353b76fdc88ceca0393e9953
cjs8487 <cjschaefer1999@gmail.com>
2020-03-29 20:27:16 -0400

    basic account creation page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Matches from './faciliation/Matches.js'
+import Create from './faciliation/Create.js'
 
 function App() {
   return (
-    <Matches />
+    <Create />
   );
 }
 
---
+++ src/faciliation/Create.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Create() {
+    return (
+        <div className="faciliationNewAccount">
+            <header>
+                <h1 className="headerLogo">Driftr</h1>
+                <h1>New Account</h1>
+            </header>
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default Create;
