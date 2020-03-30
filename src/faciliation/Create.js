2f1ecea9b5b216e749645d0d9ce2656984e5c07a
cjs8487 <cjschaefer1999@gmail.com>
2020-03-29 20:41:31 -0400

    added account creation form

--- src/faciliation/Create.js
+++ src/faciliation/Create.js
@@ -7,8 +7,15 @@ function Create() {
                 <h1 className="headerLogo">Driftr</h1>
                 <h1>New Account</h1>
             </header>
-            <form>
-
+            <form className="faciliaitonAccountForm">
+                <input type="radio" id="parent" name="accountType" value="parent"></input>
+                <label for="parent">Parent</label>
+                <input type="radio" id="child" name="accountType" value="child"></input>
+                <label for="child">Child</label><br/>
+                <label for="email">Email</label>
+                <input type="email" id="email"></input><br/>
+                <input type="button" id="accountCancel" value="Cancel"></input> <br/>
+                <input type="submit" id="accountSubmit" value="Create Account"></input>
             </form>
         </div>
     )
