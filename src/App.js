4a383ef7ff8fe72b2bf04812a927e177b4dc00a5
matedro <matedro1@gmail.com>
2020-04-14 20:11:50 -0400

    Unmodified App

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-import Grid from "@material-ui/core/Grid";
-import ProfileSummary from "./evaluation/ProfileSummary.js";
-import EvalDetailChild from "./evaluation/EvalDetailChild.js";
-
 function App() {
-  return (
-    <div className="App">
-      <EvalDetailChild />
-    </div>
-  );
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 }
 
 export default App;
