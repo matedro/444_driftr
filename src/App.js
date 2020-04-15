00867934b8534ab285a038f7cb404a4e581a57ab
matedro <matedro1@gmail.com>
2020-04-14 23:47:21 -0400

    Added Router to App, renamed "faciliation" directory

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Homepage from "./homepage";
+import Login from "./login";
+import Register from "./register";
+import Adult from "./profile/Adult";
+import Child from "./profile/Child";
+import EvalParent from "./evaluation/EvalParent";
+import EvalDetailParent from "./evaluation/EvalDetailParent";
+import EvalChild from "./evaluation/EvalChild";
+import EvalDetailChild from "./evaluation/EvalDetailChild";
+import Matches from "./facilitation/Matches";
+import Create from "./facilitation/Create";
 
 function App() {
-return (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/create-account">
+          <Register />
+        </Route>
+        {/* parents' perspective */}
+        <Route path="/p/create-profile">
+          <Adult />
+        </Route>
+        <Route path="/p/evaluate">
+          <EvalParent />
+        </Route>
+        <Route path="/p/evaluate/detail">
+          <EvalDetailParent />
+        </Route>
+        {/* children's perspective */}
+        <Route path="/c/create-profile">
+          <Child />
+        </Route>
+        <Route path="/c/evaluate">
+          <EvalChild />
+        </Route>
+        <Route path="/c/evaluate/detail">
+          <EvalDetailChild />
+        </Route>
+        {/* facilitators' perspective */}
+        <Route path="/f/matches">
+          <Matches />
+        </Route>
+        <Route path="/f/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
