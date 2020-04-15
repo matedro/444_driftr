af4eee292683abba1c3d035e29d454b3c4db5c82
matedro <matedro1@gmail.com>
2020-04-15 10:46:08 -0400

    Saving new profile redirects to eval page

--- src/profile/Adult.js
+++ src/profile/Adult.js
@@ -1,65 +1,77 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 import './Adult.css';
 import Man from './mannn.jpg';
-function Adult() { return (
-<div className="profileAdult">
-    <header>
+
+function Adult() {
+  let history = useHistory();
+
+  function handleSave() {
+    history.push("/p/evaluate");
+  }
+
+  return (
+    <div className="profileAdult">
+      <header>
         <h1>Parent Profile Creation</h1>
         <input type="submit" value="Logout" />
-    </header>
-        <img src={Man} alt="profile image" width="200" height="160" class="man"/>
+      </header>
+      <img src={Man} alt="profile image" width="200" height="160" class="man"/>
 
-    <div className="inputContainer2">
+      <div className="inputContainer2">
 
-    </div>
+      </div>
 
-    <div className="bigWrapper">
-            <div className="inputRight">
-                <form>
-                    <label>
-                        Name:
-                        <input type="text" name="name" />
-                    </label>
-                </form>
-            </div>
-            <div className="inputRight">
+      <div className="bigWrapper">
+        <div className="inputRight">
+          <form>
+            <label>
+              Name:
+              <input type="text" name="name" />
+            </label>
+          </form>
+        </div>
+        <div className="inputRight">
 
-                <form>
-                    <label>
-                        Age:
-                        <input type="text" name="name" />
-                    </label>
-                </form>
-            </div>
-            <div className="inputRight">
-                <form>
-                    <label>
-                        Interests:
-                        <input type="text" name="name" />
-                    </label>
-                </form>
-            </div>
-            <div className="inputRight">
-                <form>
-                    <label>
-                        Family Environment:
-                        <input type="text" name="name" />
-                    </label>
-                </form>
-            </div>
-            <div className="inputRight">
-                    <form>
-                        <label>
-                            Extra Information:
-                            <input type="text" name="name" />
-                        </label>
-                    </form>
-            </div>
-    </div>
-    <div className="button">
+          <form>
+            <label>
+              Age:
+              <input type="text" name="name" />
+            </label>
+          </form>
+        </div>
+        <div className="inputRight">
+          <form>
+            <label>
+              Interests:
+              <input type="text" name="name" />
+            </label>
+          </form>
+        </div>
+        <div className="inputRight">
+          <form>
+            <label>
+              Family Environment:
+              <input type="text" name="name" />
+            </label>
+          </form>
+        </div>
+        <div className="inputRight">
+          <form>
+            <label>
+              Extra Information:
+              <input type="text" name="name" />
+            </label>
+          </form>
+        </div>
+      </div>
+      <div className="button">
         <input type="submit" value="Download Questionnaire" />
         <input type="submit" value="Upload" />
-        <input type="submit" value="Save" />
+        <input type="submit" value="Save" onClick={handleSave} />
+      </div>
     </div>
-</div>
-); } export default Adult;
+  );
+}
+
+export default Adult;
